c2d456fc3e12d2e476f861c2bc85695198db9ee5
rafael <rafaelbordoni00@gmail.com>
2022-06-26 18:08:53 -0300

    Bonus: add html strip on validation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import joi from 'joi';
 import cors from 'cors';
 import dayjs from 'dayjs';
+import { stripHtml } from 'string-strip-html';
 
 dotenv.config();
 
@@ -11,7 +12,9 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-// joi validation schemas
+
+
+// Validation -----------------------------------------------------------------
 const participantSchema = joi.object({
     name: joi.string().required()
 });
@@ -22,15 +25,31 @@ const messageSchema = joi.object({
     type: joi.string().valid('message', 'private_message').required()
 });
 
-// MongoDB
+function objectStripHtml (obj) {
+    const entries = Object.entries(obj);
+    for (let i = 0; i < entries.length; i++) {
+        if (typeof entries[i][1] === 'string') {
+            entries[i][1] = stripHtml(entries[i][1]).result;
+        }
+    }
+    return Object.fromEntries(entries);
+}
+// ----------------------------------------------------------------------------
+
+
+
+// MongoDB --------------------------------------------------------------------
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient.connect().then(() => {
     db = mongoClient.db('batepapo-uol');
 });
+// ----------------------------------------------------------------------------
 
-// Participants routes
+
+
+// Participants routes --------------------------------------------------------
 server.get('/participants', async (req, res) => {
     try {
         const data = await db.collection('participants').find().toArray();
@@ -42,7 +61,8 @@ server.get('/participants', async (req, res) => {
 });
 
 server.post('/participants', async (req, res) => {
-    const validation = participantSchema.validate(req.body);
+    const body = objectStripHtml(req.body);
+    const validation = participantSchema.validate(body);
     if (validation.error) {
         res.sendStatus(422);
         return;
@@ -50,7 +70,7 @@ server.post('/participants', async (req, res) => {
 
     try {
         const data = await db.collection('participants').findOne({
-            'name': req.body.name
+            'name': body.name
         });
         if (data) {
             res.sendStatus(409);
@@ -63,7 +83,7 @@ server.post('/participants', async (req, res) => {
 
     try {
         await db.collection('participants').insertOne({
-            'name': req.body.name,
+            'name': body.name,
             'lastStatus': Date.now()
         });
     } catch (err) {
@@ -73,7 +93,7 @@ server.post('/participants', async (req, res) => {
 
     try {
         await db.collection('messages').insertOne({
-            'from': req.body.name,
+            'from': body.name,
             'to': 'Todos',
             'text': 'entra na sala...',
             'type': 'status',
@@ -86,8 +106,11 @@ server.post('/participants', async (req, res) => {
 
     res.sendStatus(201);
 });
+// ----------------------------------------------------------------------------
 
-// Messages routes
+
+
+// Messages routes ------------------------------------------------------------
 server.get('/messages', async (req, res) => {
     const limit = parseInt(req.query.limit);
     const { user } = req.headers;
@@ -120,7 +143,8 @@ server.get('/messages', async (req, res) => {
 });
 
 server.post('/messages', async (req, res) => {
-    const validation = messageSchema.validate(req.body);
+    const body = objectStripHtml(req.body);
+    const validation = messageSchema.validate(body);
     if (validation.error) {
         res.sendStatus(422);
         return;
@@ -143,9 +167,9 @@ server.post('/messages', async (req, res) => {
     try {
         await db.collection('messages').insertOne({
             'from': user,
-            'to': req.body.to,
-            'text': req.body.text,
-            'type': req.body.type,
+            'to': body.to,
+            'text': body.text,
+            'type': body.type,
             'time': dayjs().format('HH:mm:ss')
         });
     } catch (err) {
@@ -155,8 +179,11 @@ server.post('/messages', async (req, res) => {
     
     res.sendStatus(201);
 })
+// ----------------------------------------------------------------------------
 
-// Status routes and inactive users remotion
+
+
+// Status routes and inactive users remotion ----------------------------------
 server.post('/status', async (req, res) => {
     const { user } = req.headers;
     let id;
@@ -172,7 +199,6 @@ server.post('/status', async (req, res) => {
             return;
         }
     } catch (err) {
-        console.log('erro no primeiro try');
         res.sendStatus(500);
         return;
     }
@@ -183,8 +209,6 @@ server.post('/status', async (req, res) => {
             { $set: { lastStatus: Date.now() } }
         );
     } catch (err) {
-        console.log(id);
-        console.log('erro no segundo try');
         res.sendStatus(500);
         return;
     }
@@ -192,6 +216,47 @@ server.post('/status', async (req, res) => {
     res.sendStatus(200);
 });
 
+setInterval(async () => {
+    const now = Date.now();
+    let users;
+
+    try {
+        const data = await db.collection('participants').find().toArray();
+        users = [...data];
+    } catch (err) {
+        return;
+    }
+
+    for (let i = 0; i < users.length; i++) {
+        if (now - users[i].lastStatus > 10000) {
+            try {
+                await db.collection('participants').deleteOne({
+                    _id: users[i]._id
+                });
+            } catch (err) {
+                return;
+            }
+
+            try {
+                await db.collection('messages').insertOne({
+                    'from': users[i].name,
+                    'to': 'Todos',
+                    'text': 'sai da sala...',
+                    'type': 'status',
+                    'time': dayjs().format('HH:mm:ss')
+                });
+            } catch (err) {
+                return;
+            }
+        }
+    }
+
+}, 15000)
+// ----------------------------------------------------------------------------
+
+
+
+// Server listen --------------------------------------------------------------
 server.listen(5000, () => {
     console.log('Listening at port 5000...');
 });
