1b6be8bc1f15786df930d15666d3c4b0cbccce65
rafael <rafaelbordoni00@gmail.com>
2022-06-26 04:43:31 -0300

    Add get and post on /messages route

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const participantSchema = joi.object({
     name: joi.string().required()
 });
 
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message', 'private_message').required()
+});
+
 // MongoDB
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
@@ -24,13 +30,14 @@ mongoClient.connect().then(() => {
     db = mongoClient.db('batepapo-uol');
 });
 
-// Participant routes
+// Participants routes
 server.get('/participants', async (req, res) => {
     try {
         const data = await db.collection('participants').find().toArray();
         res.status(200).send(data);
     } catch (err) {
-        res.sendStatus(err.response.status);
+        res.sendStatus(500);
+        return;
     }
 });
 
@@ -60,25 +67,94 @@ server.post('/participants', async (req, res) => {
             'lastStatus': Date.now()
         });
     } catch (err) {
-        res.sendStatus(err.response.status);
+        res.sendStatus(500);
+        return;
     }
 
     try {
-        const time = dayjs().format('HH:mm:ss');
         await db.collection('messages').insertOne({
-            'from': 'xxx',
+            'from': req.body.name,
             'to': 'Todos',
             'text': 'entra na sala...',
             'type': 'status',
-            'time': time
-        });
+            'time': dayjs().format('HH:mm:ss')
+        })
     } catch (err) {
-        res.sendStatus(err.response.status);
+        res.sendStatus(500);
         return;
     }
 
     res.sendStatus(201);
 });
+
+// Messages routes
+server.get('/messages', async (req, res) => {
+    const limit = parseInt(req.query.limit);
+    const { user } = req.headers;
+    const dbQuery = { $or: [
+        { from: user },
+        { to: {$in: [user, 'Todos']} }
+    ]};
+    const dbSort = { time: -1 };
+    let data;
+
+    try {
+        if (limit) {
+            data = await db.collection('messages')
+                .find(dbQuery)
+                .sort(dbSort)
+                .limit(limit)
+                .toArray();
+        } else {
+            data = await db.collection('messages')
+                .find(dbQuery)
+                .sort(dbSort)
+                .toArray();
+        }
+    } catch (err) {
+        res.sendStatus(500);
+        return;
+    }
+
+    res.status(200).send(data);
+});
+
+server.post('/messages', async (req, res) => {
+    const validation = messageSchema.validate(req.body);
+    if (validation.error) {
+        res.sendStatus(422);
+        return;
+    }
+
+    const { user } = req.headers;
+    try {
+        const data = db.collection('participants').findOne({
+            name: user
+        });
+        if (!data) {
+            res.sendStatus(422);
+            return;
+        }
+    } catch (err) {
+        res.sendStatus(500);
+        return;
+    }
+
+    try {
+        db.collection('messages').insertOne({
+            'from': user,
+            'to': req.body.to,
+            'text': req.body.text,
+            'type': req.body.type,
+            'time': dayjs().format('HH:mm:ss')
+        });
+    } catch (err) {
+        res.sendStatus(500);
+        return;
+    }
+    
+    res.sendStatus(201);
+})
 
 server.listen(5000, () => {
     console.log('Listening at port 5000...');
